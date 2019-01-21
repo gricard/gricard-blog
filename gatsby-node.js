491c6f8763a5e82938bf431a9e7d72b180e0840b
Gabriel Ricard <4550801+gricard@users.noreply.github.com>
2019-01-21 07:10:13 -0500

    Added tag pages Change blog posts to use frontmatter.path for url

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,64 +1,103 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const _ = require('lodash')
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+    const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  return graphql(
-    `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
+    const blogPost = path.resolve(`./src/templates/blog-post.js`)
+    return graphql(
+        `
+            {
+                allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
+                    edges {
+                        node {
+                            fields {
+                                slug
+                            }
+                            frontmatter {
+                                path
+                                title
+                                tags
+                            }
+                        }
+                    }
+                }
             }
-          }
+        `
+    ).then(result => {
+        if (result.errors) {
+            throw result.errors
         }
-      }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
 
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
+        // Create blog posts pages.
+        const posts = result.data.allMarkdownRemark.edges
 
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
+        posts.forEach((post, index) => {
+            const previous = index === posts.length - 1 ? null : posts[index + 1].node
+            const next = index === 0 ? null : posts[index - 1].node
 
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
+            createPage({
+                //path: post.node.fields.slug,
+                path: post.node.frontmatter.path,
+                component: blogPost,
+                context: {
+                    slug: post.node.fields.slug,
+                    previous,
+                    next,
+                },
+            })
+        })
+
+        // Tag pages:
+        const tagTemplate = path.resolve('src/templates/tag-post-list.js')
+        let tagList = []
+        // Iterate through each post, putting all found tags into `tags`
+        posts.forEach(edge => {
+            let {
+                node: {
+                    frontmatter: { tags },
+                },
+            } = edge
+            if (tags) {
+                tagList = tagList.concat(tags)
+            }
+        })
+        // Eliminate duplicate tags
+        tagList = [...new Set(tagList)]
+
+        // add index page
+        const tagPage = path.resolve('src/pages/tag-list.js')
+        createPage({
+            path: '/tags',
+            component: tagPage,
+            context: {
+                tags: tagList.sort(),
+            },
+        })
+
+        // Make tag pages
+        tagList.forEach(tag => {
+            createPage({
+                path: `/tags/${_.kebabCase(tag)}/`,
+                component: tagTemplate,
+                context: {
+                    tag,
+                },
+            })
+        })
     })
-  })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+    const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
+    if (node.internal.type === `MarkdownRemark`) {
+        const value = createFilePath({ node, getNode })
+        createNodeField({
+            name: `slug`,
+            node,
+            value,
+        })
+    }
 }
