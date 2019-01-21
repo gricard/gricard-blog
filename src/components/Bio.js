9dfcae67d0fb4e4b44ddeef7d7dc9771a6c60538
Gabriel Ricard <4550801+gricard@users.noreply.github.com>
2019-01-21 06:58:40 -0500

    Formatting update

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,61 +5,61 @@ import Image from 'gatsby-image'
 import { rhythm } from '../utils/typography'
 
 function Bio() {
-  return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        const { author, social } = data.site.siteMetadata
-        return (
-          <div
-            style={{
-              display: `flex`,
-              marginBottom: rhythm(2.5),
+    return (
+        <StaticQuery
+            query={bioQuery}
+            render={data => {
+                const { author, social } = data.site.siteMetadata
+                return (
+                    <div
+                        style={{
+                            display: `flex`,
+                            marginBottom: rhythm(2.5),
+                        }}
+                    >
+                        <Image
+                            fixed={data.avatar.childImageSharp.fixed}
+                            alt={author}
+                            style={{
+                                marginRight: rhythm(1 / 2),
+                                marginBottom: 0,
+                                minWidth: 50,
+                                borderRadius: `100%`,
+                            }}
+                            imgStyle={{
+                                borderRadius: `50%`,
+                            }}
+                        />
+                        <p>
+                            My thoughts on random things, like running, fitness, paintball, life, etc. I also rant about
+                            software over at <a href="https://cdgd.tech">cdgd.tech</a>, and{' '}
+                            <a href={`https://twitter.com/${social.twitter}`}>twitter</a>.
+                        </p>
+                    </div>
+                )
             }}
-          >
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
-            />
-            <p>
-              My thoughts on random things, like running, fitness, paintball, life, etc.
-              I also rant about software over at <a href="https://cdgd.tech">cdgd.tech</a>,
-              and <a href={`https://twitter.com/${social.twitter}`}>twitter</a>.
-            </p>
-          </div>
-        )
-      }}
-    />
-  )
+        />
+    )
 }
 
 const bioQuery = graphql`
-  query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-      childImageSharp {
-        fixed(width: 50, height: 50) {
-          ...GatsbyImageSharpFixed
+    query BioQuery {
+        avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+            childImageSharp {
+                fixed(width: 50, height: 50) {
+                    ...GatsbyImageSharpFixed
+                }
+            }
         }
-      }
-    }
-    site {
-      siteMetadata {
-        author
-        social {
-          twitter
+        site {
+            siteMetadata {
+                author
+                social {
+                    twitter
+                }
+            }
         }
-      }
     }
-  }
 `
 
 export default Bio
