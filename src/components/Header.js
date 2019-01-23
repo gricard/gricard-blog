1f6bd2f7c2449b41f1cf16f61bbf3acd769a9381
Gabriel Ricard <4550801+gricard@users.noreply.github.com>
2019-01-23 13:57:08 -0500

    Fix: window is not defined

--- src/components/Header.js
+++ src/components/Header.js
@@ -65,8 +65,8 @@ const SiteHeader = props => (
                 {props.title}
                 </h1>
             </LogoLink>
-            {window.location.pathname === '/' ? null : <NavLink to="/">Blog</NavLink>}
-            {window.location.pathname === '/gear' ? null : <NavLink to="/gear">Gear</NavLink>}
+            {typeof window !== 'undefined' && window.location.pathname === '/' ? null : <NavLink to="/">Blog</NavLink>}
+            {typeof window !== 'undefined' && window.location.pathname === '/gear' ? null : <NavLink to="/gear">Gear</NavLink>}
         </Nav>
     </Header>
 );
