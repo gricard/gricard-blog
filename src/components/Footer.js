9edc71c93dcf6860635131896f46e3be77535698
Gabriel Ricard <4550801+gricard@users.noreply.github.com>
2019-01-23 16:38:34 -0500

    Made separate Footer component Remove old header code in layout Pass location to Header so it doesn't need window.location

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+    <footer style={{textAlign: 'center'}}>
+        © {new Date().getFullYear()} Gabriel Ricard. All rights reserved.
+        <br /> 
+        Built with {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </footer>
+
+);
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,60 +3,15 @@ import { Link } from 'gatsby'
 
 import { rhythm, scale } from '../utils/typography'
 import Header from './Header'
+import Footer from './Footer'
 
 class Layout extends React.Component {
     render() {
         const { location, title, children } = this.props
         const rootPath = `${__PATH_PREFIX__}/`
-        let header
-
-        if (location.pathname === rootPath) {
-            header = (
-                <h1
-                    style={{
-                        ...scale(1.5),
-                        marginBottom: rhythm(1.5),
-                        marginTop: 0,
-                        fontFamily: `Lobster, sans-serif`,
-                    }}
-                >
-                    <Link
-                        style={{
-                            boxShadow: `none`,
-                            textDecoration: `none`,
-                            color: `inherit`,
-                        }}
-                        to={`/`}
-                    >
-                        {title}
-                    </Link>
-                </h1>
-            )
-        } else {
-            header = (
-                <h3
-                    style={{
-                        fontFamily: `Lobster, sans-serif`,
-                        marginTop: 0,
-                    }}
-                >
-                    <Link
-                        style={{
-                            boxShadow: `none`,
-                            textDecoration: `none`,
-                            color: `inherit`,
-                        }}
-                        to={`/`}
-                    >
-                        {title}
-                    </Link>
-                </h3>
-            )
-        }
-
-        header = (
-            <Header role="banner" title={title} />
-        )
+        const header = (
+            <Header role="banner" title={title} location={location} />
+        );
 
         return (
             <div
@@ -71,12 +26,7 @@ class Layout extends React.Component {
                 <div style={{paddingTop: '60px'}}>
                 {children}
                 </div>
-                <footer style={{textAlign: 'center'}}>
-                    © {new Date().getFullYear()} Gabriel Ricard
-                    <br /> 
-                    Built with {` `}
-                    <a href="https://www.gatsbyjs.org">Gatsby</a>
-                </footer>
+                <Footer />
             </div>
         )
     }
