a0de3bbafd5284a5c9519deeffb0adc70a2ba895
Gabriel Ricard <4550801+gricard@users.noreply.github.com>
2019-01-24 08:20:31 -0500

    Added Disqus comments

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import _kebabCase from 'lodash/kebabCase'
+import { DiscussionEmbed } from 'disqus-react'
 
 import Bio from '../components/Bio'
 import Layout from '../components/Layout'
@@ -12,6 +13,11 @@ class BlogPostTemplate extends React.Component {
         const post = this.props.data.markdownRemark
         const siteTitle = this.props.data.site.siteMetadata.title
         const { previous, next } = this.props.pageContext
+        const disqusShortname = 'gricard-run';
+        const disqusConfig = {
+          identifier: post.id,
+          title: post.frontmatter.title,
+        };
 
         return (
             <Layout location={this.props.location} title={siteTitle}>
@@ -69,6 +75,8 @@ class BlogPostTemplate extends React.Component {
                         )}
                     </li>
                 </ul>
+
+                <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
             </Layout>
         )
     }
